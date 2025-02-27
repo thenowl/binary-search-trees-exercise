4f94a2b19d8ac46e4cd32fd1532a719b3d77dabc
thenowl <183875071+thenowl@users.noreply.github.com>
2025-02-27 13:45:44 +0100

    Outline scope of the project

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ class Tree {
     this.root = this.buildTreePrep(this.mergeSort(array));
   }
 
+  // Sort unsorted array in preparation:
+
   mergeSort(array) {
     if (array.length === 0) return;
     if (array.length === 1) return array;
@@ -41,6 +43,8 @@ class Tree {
     return sorted;
   }
 
+  // Create Balanced Binary Search Tree:
+
   buildTree(array, start, end) {
     if (start > end) return null;
 
@@ -53,12 +57,36 @@ class Tree {
     return root;
   }
 
+  // Prepare array by removing duplicates and passing start/end args to buildTree():
+
   buildTreePrep(array) {
     const duplicatesRemoved = array.filter(function (item, pos) {
       return array.indexOf(item) == pos;
     });
     return this.buildTree(duplicatesRemoved, 0, duplicatesRemoved.length - 1);
   }
+
+  insert(value) {}
+
+  deleteItem(value) {}
+
+  find(value) {}
+
+  levelOrder(callback) {}
+
+  inOrder(callback) {}
+
+  preOrder(callback) {}
+
+  postOrder(callback) {}
+
+  height(node) {}
+
+  depth(node) {}
+
+  isBalanced() {}
+
+  rebalance() {}
 }
 
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
